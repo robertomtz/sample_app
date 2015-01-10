f394d5f1dcd1aea8ab63618afa53c3f49204c5f4
Roberto Mtz <roberto@robertomtz.com>
2015-01-10 03:41:50 -0600

    Termina los ejercicios del capitulo 9

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -4,6 +4,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @second = users(:archer)
   end
 
   test "unsuccessful edit" do
@@ -50,4 +51,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
           assert_equal @user.name,  name
           assert_equal @user.email, email
         end
+
+        test "successful edit with friendly forwarding but not the next time" do
+          get edit_user_path(@user)
+          log_in_as(@user)
+          assert_redirected_to edit_user_path(@user)
+          name  = "Foo Bar"
+          email = "foo@bar.com"
+          patch user_path(@user), user: { name:  name,
+            email: email,
+            password:              "foobar",
+            password_confirmation: "foobar" }
+            assert_not flash.empty?
+            assert_redirected_to @user
+            @user.reload
+            assert_equal @user.name,  name
+            assert_equal @user.email, email
+          log_in_as(@second)
+          assert_redirected_to @second
+          end
   end
