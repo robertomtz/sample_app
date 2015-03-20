b0f21e15ed069c0dc322ed4f1a6d39dbe7e163f1
Roberto Mtz <roberto@robertomtz.com>
2015-03-20 11:35:16 -0600

    Finished excercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,13 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
-  end
+      @users = User.where(activated: true).paginate(page: params[:page])
+    end
 
   def show
-    @user = User.find(params[:id])
-    #debugger
-  end
+      @user = User.find(params[:id])
+      redirect_to root_url and return unless @user.activated?
+    end
 
   def new
     @user = User.new
